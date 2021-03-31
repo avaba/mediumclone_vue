2a44557d65ce146053b4e50a52de1d017a05a039
Иголышков Сергей <sigolyshkov@yandex.ru>
2021-04-01 01:54:24 +0300

    Registor page

---
+++ src/views/Register.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign Up</h1>
+          <p class="text-xs-center">
+            <router-link :to="{name: 'login'}">Need an account?</router-link>
+          </p>
+          <p>Walidation errors</p>
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Username"
+              />
+            </fieldset>
+
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+              />
+            </fieldset>
+
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="isSubmitting"
+            >
+              Sign app
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'McvRegister',
+    computed: {
+      isSubmitting() {
+        return this.$store.state.auth.isSubmitting
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('onSubmit')
+        this.$store.dispatch('register')
+      }
+    }
+  }
+</script>
